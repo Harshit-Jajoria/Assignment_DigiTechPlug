cb4e23f3bbcbc6d260c51ee43a6faf2b5b3175f4
Harshit-Jajoria <harshitjajoria66@gmail.com>
2023-07-05 11:02:13 +0530

    edits

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
 import './index.css';
 
@@ -10,13 +10,29 @@ const App = () => {
     command: '',
   });
 
+  const [tag, setTag] = useState('p');
+  const undoStackRef = useRef([]);
+  const redoStackRef = useRef([]);
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
+
+    // Push the previous command to the undo stack
+    undoStackRef.current.push(inputValues.command);
+
+    // Clear the redo stack whenever a new change is made
+    redoStackRef.current = [];
+
     setInputValues((prevState) => ({
       ...prevState,
       [name]: value,
     }));
   };
+
+  const handleTagChange = (selectedTag) => {
+    setTag(selectedTag);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     const savedData = await axios.post(
@@ -30,11 +46,34 @@ const App = () => {
     );
     setInputValues({
       content: '',
-    tone: '',
-    keywords: '',
-    command: '',
-    })
+      tone: '',
+      keywords: '',
+      command: '',
+    });
     console.log(savedData.data);
+    alert(' Data has been saved successfully to DataBase')
+  };
+
+  const handleUndo = () => {
+    if (undoStackRef.current.length > 0) {
+      const previousValue = undoStackRef.current.pop();
+      redoStackRef.current.push(inputValues.command);
+      setInputValues((prevState) => ({
+        ...prevState,
+        command: previousValue,
+      }));
+    }
+  };
+
+  const handleRedo = () => {
+    if (redoStackRef.current.length > 0) {
+      const nextValue = redoStackRef.current.pop();
+      undoStackRef.current.push(inputValues.command);
+      setInputValues((prevState) => ({
+        ...prevState,
+        command: nextValue,
+      }));
+    }
   };
 
   return (
@@ -76,34 +115,82 @@ const App = () => {
         </div>
       </div>
       <div className="column2">
-        <div className="nav flex  ">
+        <div className="nav flex">
           <div className="shift flex space">
-            <div className="undo space">↶</div>
-            <div className="redo space">↷ </div>
+            <div className="undo space" onClick={handleUndo}>
+              ↶
+            </div>
+            <div className="redo space" onClick={handleRedo}>
+              ↷
+            </div>
           </div>
 
-          <div className="fontsize flex ">
-            <div className="h1 space">H1</div>
-            <div className="h2 space">H2</div>
-            <div className="h3 space">H3</div>
-            <div className="h4 space">H4 </div>
+          <div className="fontsize flex">
+            <div
+              className="h1 space"
+              onClick={() => handleTagChange('h1')}
+              style={{ textDecoration: tag === 'h1' ? 'underline' : 'none' }}
+            >
+              H1
+            </div>
+            <div
+              className="h2 space"
+              onClick={() => handleTagChange('h2')}
+              style={{ textDecoration: tag === 'h2' ? 'underline' : 'none' }}
+            >
+              H2
+            </div>
+            <div
+              className="h3 space"
+              onClick={() => handleTagChange('h3')}
+              style={{ textDecoration: tag === 'h3' ? 'underline' : 'none' }}
+            >
+              H3
+            </div>
+            <div
+              className="h4 space"
+              onClick={() => handleTagChange('h4')}
+              style={{ textDecoration: tag === 'h4' ? 'underline' : 'none' }}
+            >
+              H4
+            </div>
           </div>
 
-          <div className="fontstyle flex ">
-            <div className="bold space">
+          <div className="fontstyle flex">
+            <div
+              className="bold space"
+              onClick={() => handleTagChange('bold')}
+              style={{ textDecoration: tag === 'bold' ? 'underline' : 'none' }}
+            >
               <b>B</b>
             </div>
-            <div className="italics space">
+            <div
+              className="italics space"
+              onClick={() => handleTagChange('italics')}
+              style={{ textDecoration: tag === 'italics' ? 'underline' : 'none' }}
+            >
               <i>I</i>
             </div>
-            <div className="underline space">U</div>
+            <div
+              className="underline space"
+              onClick={() => handleTagChange('underline')}
+              style={{ textDecoration: tag === 'underline' ? 'underline' : 'none' }}
+            >
+              U
+            </div>
           </div>
         </div>
         <div className="display">
           <div className="col1">
-            <p>{inputValues.command}</p>
+            {tag === 'p' && <p>{inputValues.command}</p>}
+            {tag === 'h1' && <h1>{inputValues.command}</h1>}
+            {tag === 'h2' && <h2>{inputValues.command}</h2>}
+            {tag === 'h3' && <h3>{inputValues.command}</h3>}
+            {tag === 'h4' && <h4>{inputValues.command}</h4>}
+            {tag === 'bold' && <b>{inputValues.command}</b>}
+            {tag === 'italics' && <i>{inputValues.command}</i>}
+            {tag === 'underline' && <u>{inputValues.command}</u>}
           </div>
-
           <div className="col2">
             <div className="bg">
               <div className="c1">
